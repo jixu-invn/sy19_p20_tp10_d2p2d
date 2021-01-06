0e6d7dbb9aecb7f944f0f2233f939491e57691b9
Jiali Xu <garyxu1001@gmail.com>
2021-01-06 20:45:25 +0100

    Phoneme Livrable

--- Livrable/prediction.R
+++ Livrable/prediction.R
@@ -1,11 +1,26 @@
 prediction_phoneme <- function(dataset) {
   # Chargement de l’environnement
-  load("env.Rdata")
-  # Mon algorithme qui renvoie les prédictions sur le jeu de données
-  # ‘dataset‘ fourni en argument.
-  # ...
-  return(predictions)
+  library(tensorflow)
+  library(keras)
+  load("phoneme.Rdata") # load res.pca
+  model.serialize <- serialize_model(load_model_hdf5("phoneme.h5"))
+  model <- unserialize_model(model.serialize)
+  
+  classes <- c("aa", "ao", "dcl","iy", "sh") 
+  
+  # Transformation ACP
+  nb.p <- ncol(dataset)
+  dataset.pca <- as.data.frame((scale(dataset[, -257], center=res.pca$center) %*% res.pca$rotation)[, 1:res.pca$n])
+  
+  # Prédiction
+  pred <- predict(model, as.matrix(dataset.pca))
+  predictions <- rep(0, nrow(dataset))
+  for (i in 1:nrow(dataset)) {
+    predictions[i] <- classes[which.max(pred[i,])]
+  }
+  return(as.factor(predictions))
 }
+
 prediction_letter <- function(dataset) {
   # Chargement de l’environnement
   load("env.Rdata")
@@ -14,6 +29,7 @@ prediction_letter <- function(dataset) {
   # ...
   return(predictions)
 }
+
 prediction_bike <- function(list) {
   # Chargement de l’environnement
   load("env.Rdata")

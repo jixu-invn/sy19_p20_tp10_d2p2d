85eca881b0fb38c46268f9f23ac8160572a5facf
Jiali Xu <garyxu1001@gmail.com>
2021-01-06 19:49:41 +0100

    add Livrable

---
+++ Livrable/prediction.R
@@ -0,0 +1,24 @@
+prediction_phoneme <- function(dataset) {
+  # Chargement de l’environnement
+  load("env.Rdata")
+  # Mon algorithme qui renvoie les prédictions sur le jeu de données
+  # ‘dataset‘ fourni en argument.
+  # ...
+  return(predictions)
+}
+prediction_letter <- function(dataset) {
+  # Chargement de l’environnement
+  load("env.Rdata")
+  # Mon algorithme qui renvoie les prédictions sur le jeu de données
+  # ‘dataset‘ fourni en argument.
+  # ...
+  return(predictions)
+}
+prediction_bike <- function(list) {
+  # Chargement de l’environnement
+  load("env.Rdata")
+  # Mon algorithme qui renvoie les prédictions sur le jeu de données
+  # ‘dataset‘ fourni en argument.
+  # ... # ...
+  return(predictions)
+}
